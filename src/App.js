c08b9cf8472ae734d49f30e954a93dbb6f75b3de
saadwaqas688 <saadwaqas688@gmail.com>
2023-05-18 19:27:50 +0500

    Your commit message

--- src/App.js
+++ src/App.js
@@ -21,7 +21,16 @@ function App() {
     };
 
     setStudents([...students, studentObject]);
-    console.log(studentObject);
+    
+  }
+
+
+  function deleteStudent(rollNumber) {
+    alert(rollNumber)
+    let tempStudents=[...students]
+     tempStudents=tempStudents.filter((element)=>element.rollNumber!==rollNumber)
+    setStudents(tempStudents);
+    
   }
 
   return (
@@ -42,15 +51,23 @@ function App() {
         onChange={handleChangeStudentRollNumber}
       />
       <br></br>
-      <button onClick={addStudent}>Add Student</button>
+      <button style={{marginLeft:"5px",background:"blue"}} onClick={addStudent}>Add Student</button>
+
       <div>
+        <table>
       {
         students.map((element)=> {
           return(
+            <div style={{display:"flex",marginTop:"50px"}}>
             <div> {element.name}</div>
+            <button style={{marginLeft:"5px",background:"green"}} onClick={()=>deleteStudent(element.rollNumber)}>delete</button>
+            </div>
+        
+
           )
         })
       }
+      </table>
       </div>
    
     </div>
@@ -59,3 +76,113 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+// import { useState } from "react";
+
+// const divStyle = {
+//   display: 'flex',
+//   flexDirection: 'column',
+//   justifyContent: 'center',
+//   alignItems: 'center',
+//   backgroundColor: 'yellow',
+//   width: '100%',
+//   height: '700px',
+// };
+
+// const countStyle = {
+//   marginBottom: '10px',
+//   fontSize: '100px',
+// };
+
+
+// const buttonStyle = {
+//   marginBottom: '10px',
+//   padding: '10px 20px',
+//   backgroundColor: 'orange',
+//   color: 'white',
+//   border: 'none',
+//   borderRadius: '4px',
+//   fontSize: '16px',
+//   cursor: 'pointer',
+// };
+
+// function App() {
+//   const [count, setCount] = useState(0);
+
+
+//   function handleIncrement() {
+//     let newCount=count+1
+//     setCount(newCount);
+//   }
+
+//   function handleDecrement() {
+//     let newCount=count-1
+//     setCount(newCount); 
+//    }
+
+
+//   return (
+//     <div style={divStyle}>
+//     <h3 style={countStyle}>{count}</h3>
+//     <button style={buttonStyle} onClick={handleIncrement}>Increment</button>
+//     <button style={buttonStyle} onClick={handleDecrement}>Decrement</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+
+// import React, { useState } from 'react';
+
+// const App = () => {
+//   const [isOn, setIsOn] = useState(false);
+
+//   const bulbStyle = {
+//     width: '100px',
+//     height: '100px',
+//     borderRadius: '50%',
+//     backgroundColor: isOn ? 'yellow' : 'gray',
+//     boxShadow: isOn ? '0 0 10px yellow' : 'none',
+//     margin: '20px',
+//   };
+
+//   const buttonStyle = {
+//     padding: '10px 20px',
+//     backgroundColor: 'lightblue',
+//     color: 'white',
+//     border: 'none',
+//     borderRadius: '4px',
+//     fontSize: '16px',
+//     cursor: 'pointer',
+//     marginRight: '10px',
+//   };
+
+//   const handleTurnOn = () => {
+//     setIsOn(true);
+//   };
+
+//   const handleTurnOff = () => {
+//     setIsOn(false);
+//   };
+
+//   return (
+//     <div>
+//       <div style={bulbStyle}></div>
+//       <button style={buttonStyle} onClick={handleTurnOn}>On</button>
+//       <button style={buttonStyle} onClick={handleTurnOff}>Off</button>
+//     </div>
+//   );
+// };
+
+// export default App;
+
